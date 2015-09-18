742114c0fb7b197e88bacdab5986df2af90e1011
Arun Ramamurthy <arun.ramamurthy@berkeley.edu>
2015-09-18 11:56:47 -0700

    Submit

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,15 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(a)
+  	sum_these = Array.new 
+  	a.each{|e| 
+  		e=e.to_i
+  		if (e%2==0) && (e<=8)
+  			sum_these.push(e+2)
+  		end
+  	}
+  	sum_these = sum_these.uniq
+  	sum = 0
+  	sum_these.each{|e| sum+=e}
+  	return sum
+  end
 end
